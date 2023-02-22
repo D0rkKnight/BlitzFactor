9d8d148077317571b0e00f2c9ddb650cf6d011a7
D0rkKnight <shouhanzen@gmail.com>
2023-02-21 19:28:13 -0800

    diff

--- src/script.ts
+++
@@ -1,7 +0,0 @@
-// Sample JavaScript code
-console.log('Script loaded.');
-
-// Grab index.html and inject into body
-const index = document.createElement('div');
-
-console.log(document)
---
+++ src/script.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { useEffect } from 'react';
+import * as ReactDOM from 'react-dom';
+
+export default function App() {
+  useEffect(() => {
+    // code to execute when the component is loaded
+    console.log('Component loaded!');
+  }, []);
+
+  function handleClick() {
+    // code to execute when the button is clicked
+    console.log('Button clicked!');
+  }
+
+  return (
+    <button onClick={handleClick}> Button!!! </button>
+  );
+}
+
+console.log('testReactComponent.tsx loaded!');
+console.log(document.getElementById('app'));
+
+// Add app to DOM
+// ReactDOM.render(<App />, document.getElementById('app'));
