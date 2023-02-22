f2381cce935afdf7a95a85a0ee4815ac02e28921
D0rkKnight <shouhanzen@gmail.com>
2023-02-21 19:11:35 -0800

    document is discoverable?

--- src/blitzEditor.ts
+++ src/blitzEditor.ts
@@ -43,7 +43,7 @@ export class BlitzEditorProvider implements vscode.CustomTextEditorProvider {
 	private getHtmlForWebview(webview: vscode.Webview): string {
 		// Local path to script and css for the webview
 		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(
-			this.context.extensionUri, 'media', 'script.ts'));
+			this.context.extensionUri, 'src', 'script.ts'));
 
 		const styleUri = webview.asWebviewUri(vscode.Uri.joinPath(
 			this.context.extensionUri, 'media', 'style.css'));
--- src/script.ts
+++ src/script.ts
@@ -3,7 +3,5 @@ console.log('Script loaded.');
 
 // Grab index.html and inject into body
 const index = document.createElement('div');
-index.innerHTML = require('./index.html');
 
-// Inject into body
-document.body.appendChild(index);
+console.log(document)
