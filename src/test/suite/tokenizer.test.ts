c46a1be09353d82289dbc028e2978eefaa188d74
D0rkKnight <shouhanzen@gmail.com>
2023-03-18 22:50:36 -0700

    Renamed st test, added mock test

--- src/test/suite/tokenizer.test.ts
+++ src/test/suite/tokenizer.test.ts
@@ -1,4 +1,5 @@
-import * as assert from 'assert';
+import assert from 'assert';
+import * as sinon from 'sinon';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -22,6 +23,25 @@ suite('Syntax Tree Test Suite', () => {
 	test('Tokenizer parse', () => {
         // Build a syntax tree from a json file
         let json = Tokenizer.tokenize("let x = 5;");
-        console.log(json);
 	});
+
+    test('Spy test', () => {
+        let spy = sinon.spy(Tokenizer, 'WASMtoTREE');
+        let json = Tokenizer.tokenize("let x = 5;");
+
+        assert(spy.callCount === 8, "WASMtoTREE should be called 8 times");
+        spy.restore();
+    })
+
+    test('Mock test', () => {
+        let mock = sinon.mock(Tokenizer);
+
+        // Replaces this function call with an undefined return value
+        // And adds an expectation that it will be called at least once
+        mock.expects('WASMtoTREE').atLeast(1);
+
+        let json = Tokenizer.tokenize("let x = 5;");
+
+        mock.verify();
+    })
 });
